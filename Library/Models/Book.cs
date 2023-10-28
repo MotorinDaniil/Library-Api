12baf35772d1da6cadd6be2b960ee5ea71a88222
danch <danch@LAPTOP-6MK2PS7G>
2023-10-29 00:45:46 +0300

    Добавьте файлы проекта.

---
+++ Library/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int ISBN { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime DateTaken { get; set; }
+        public DateTime DayToReturn { get; set; }
+    }
+}
---
+++ Library/Models/BookContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Library.Models
+{
+    public class BookContext:DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Book> BooksItems { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

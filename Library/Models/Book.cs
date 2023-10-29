9398e6ea41003a8a44e95a187c328f5af4485de5
danch <danch@LAPTOP-6MK2PS7G>
2023-10-29 22:12:57 +0300

    Library Api with endpoints

--- Library/Models/Book.cs
+++ Library/Models/Book.cs
@@ -3,12 +3,12 @@
     public class Book
     {
         public int Id { get; set; }
-        public int ISBN { get; set; }
+        public string ISBN { get; set; }
         public string Name { get; set; }
         public string Genre { get; set; }
         public string Description { get; set; }
         public string Author { get; set; }
         public DateTime DateTaken { get; set; }
-        public DateTime DayToReturn { get; set; }
+        public DateTime DateToReturn { get; set; }
     }
 }
